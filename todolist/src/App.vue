00bf1188aab01379f8572e081f00c19e75fc45a2
zzj98 <1157050696@qq.com>
2019-07-25 09:39:33 +0800

    todolist

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,15 +1,44 @@
 <template>
     <div id="app">
-
+        <input type="text" v-model="msg">&nbsp;
+        <button @click="daADD">确定</button>
+        <hr/>
+        <ul>
+            <h3>正在进行的事件</h3>
+            <li v-for="(item,key) in list" v-if="!list.flag">
+                <input type="checkbox" v-model="list.flag"> {{item.title}} <button @click="deleteData(key)">删除</button>
+            </li>
+            <h3>已完成的事件</h3>
+            <li v-for="(item,key) in list" v-if="list.flag">
+                <input type="checkbox" v-model="list.flag"> {{item.title}} <button @click="deleteData(key)">删除</button>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
     export default {
         data() {
-            return {}
+            return {
+                msg:'',
+                list:[],
+            }
         },
-        methods:{}
+        methods:{
+            daADD(){
+                this.list.push({
+                    title:this.msg,
+                    flag:false,
+                });
+                this.msg = '';
+            },
+        }
     }
 </script>
+
+<style>
+    ul{
+        list-style: none;
+    }
+</style>
 
