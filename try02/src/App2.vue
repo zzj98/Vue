b2174eddb7dded8d8d250a343479dfb6196d1b39
zzj98 <1157050696@qq.com>
2019-07-25 22:26:15 +0800

    todolist

---
+++ try02/src/App2.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="app">
+        {{msg}}
+        <v-home></v-home>
+    </div>
+</template>
+
+<script>
+    import  Home from './components/Home';/*引入组件*/
+    export default {
+        data() {
+            return {
+                msg: '你好vue',
+            }
+        },
+        methods:{        },
+        components:{
+            'v-home':Home/*挂载组件*/
+        }
+    }
+</script>
---
+++ try02/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>Home</h3>
+        <h4>color</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    /*
+       scoped
+       会使样式只影响该组件
+       不在作用于全局
+       */
+    h4{
+        color: red;
+    }
+</style>
