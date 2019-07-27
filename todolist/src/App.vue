f5e09039f3f373b368f3fc4e01c02298b30c3995
zzj98 <1157050696@qq.com>
2019-07-27 23:21:51 +0800

    todolist

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,17 +1,22 @@
 <template>
     <div id="app">
-        <input type="text" v-model="msg" @keydown="enterDown">&nbsp;
-        <button @click="daADD">确定</button>
-        <hr/>
+        <input type="text" ref="txt" class="txtb" v-model="msg" @keydown="enterDown">&nbsp;
+        <button class="btn" @click="daADD">确定</button>
         <ul>
-            <h3>正在进行的事件</h3>
-            <li v-for="(item,key) in list" v-if="!item.flag">
-                <input type="checkbox" v-model="item.flag" @change="saveList"> {{item.title}} <button @click="deleteData(key)">删除</button>
-            </li>
-            <h3>已完成的事件</h3>
-            <li v-for="(item,key) in list" v-if="item.flag">
-                <input type="checkbox" v-model="item.flag" @change="saveList"> {{item.title}} <button @click="deleteData(key)">删除</button>
-            </li>
+            <div class="notcomp">
+                <h3>Not Completed</h3>
+                <li v-for="(item,key) in list" class="task" v-if="!item.flag">
+                    <input type="checkbox" v-model="item.flag" @change="saveList"> {{item.title}} <i class='fa fa-trash' @click="deleteData(key)"></i>
+                    <!--{{item.title}} <i class='fa fa-trash' @click="deleteData(key)"></i> <i class='fa fa-check' @click="saveList"></i> <button @click="deleteData(key)">删除</button>-->
+                </li>
+            </div>
+            <div class="comp">
+                <h3>Completed</h3>
+                <li v-for="(item,key) in list" class="task" v-if="item.flag">
+                    <input type="checkbox" v-model="item.flag" @change="saveList"> {{item.title}} <i class='fa fa-trash' @click="deleteData(key)"></i>
+                    <!--{{item.title}} <i @click="deleteData(key)" class='fa fa-trash'></i> <button @click="deleteData(key)">删除</button>-->
+                </li>
+            </div>
         </ul>
     </div>
 </template>
@@ -28,7 +33,7 @@
         },
         methods:{
             enterDown(e){
-                if (e.keyCode === 13 ){
+                if (e.keyCode === 13 && this.$refs.txt.value !== ""){
                     this.list.push({
                         title:this.msg,
                         flag:false,
@@ -39,13 +44,15 @@
                 storage.set('list',this.list);
             },
             daADD(){
-                this.list.push({
-                    title:this.msg,
-                    flag:false,
-                });
-                this.msg = '';
-                // localStorage.setItem('list',JSON.stringify(this.list));
-                storage.set('list',this.list);
+                if (this.$refs.txt.value !== "") {
+                    this.list.push({
+                        title:this.msg,
+                        flag:false,
+                    });
+                    this.msg = '';
+                    // localStorage.setItem('list',JSON.stringify(this.list));
+                    storage.set('list',this.list);
+                }
             },
             deleteData(key){
                 this.list.splice(key,1);
@@ -68,8 +75,68 @@
 </script>
 
 <style>
+    *{
+        margin: 0;
+        padding: 0;
+        font-family: "montserrat",sans-serif;
+        box-sizing: border-box;
+    }
+
+    body{
+        background-image: linear-gradient(120deg,#487eb0,#fbc531);
+        min-height: 100vh;
+    }
+
+    #app{
+        max-width: 800px;
+        margin: auto;
+        padding: 10px;
+    }
+
+    .txtb{
+        width: 85%;
+        border: none;
+        border-bottom: 2px solid #000;
+        background: none;
+        padding: 10px;
+        margin-left: 3px;
+        outline: none;
+        font-size: 18px;
+    }
+
+    h3{
+        margin: 10px 0;
+    }
+
+    .task{
+        width: 100%;
+        background: rgba(225,225,225,0.5);
+        padding: 18px;
+        margin: 6px 0;
+        overflow: hidden;
+    }
+
+    .task i{
+        float: right;
+        margin-left: 20px;
+        cursor: pointer;
+    }
+
+    .comp .task{
+        background: rgba(0,0,0,.5);
+        color: #fff;
+    }
     ul{
         list-style: none;
+    }
+    .btn{
+        width: 12%;
+        background: #2c3e50;
+        border: 0;
+        color: white;
+        padding: 10px 0;
+        border-radius: 6px;
+        cursor: pointer;
     }
 </style>
 
